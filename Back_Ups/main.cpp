e4323fdc3fb1c2fed87516688016555900b450b9
Rombik-dev-base <61863339+Rombik-dev-base@users.noreply.github.com>
2020-11-27 20:22:25 +0300

    Add files via upload

--- Back_Ups/main.cpp
+++ Back_Ups/main.cpp
@@ -9,16 +9,59 @@ const std::string currentDateTime() {
     return buf;
 }
 
-int main() {
+void case1(){
+    Back_Up test({"test1.txt","test2.txt"});
+    test.create_point("full");
+    test.print_point_info(0);
+    int n = 5;
+    Sleep(10000);
+    test.create_point("delta");
+    test.print_point_info(1);
+    test.create_point("full");
+    std :: cout << test.print_amount_of_points() << '\n';
+    test.delete_by_amount(2);
+    std :: cout << test.print_amount_of_points() << '\n';
+    test.delete_by_amount(1);
+    std :: cout << test.print_amount_of_points() << '\n';
+    test.print_point_info(0);
+    test.create_point("full");
+    test.print_point_info(0);
+    Sleep(10000);
+    test.create_point("delta");
+    test.print_point_info(1);
+    test.create_point("full");
+    test.delete_by_size(2100);
+    test.print_amount_of_points();
+    test.print_point_info(0);
+
+}
+
+void case2(){
+    Back_Up test({"test1.txt","test2.txt"});
+    test.create_point("full");
+    Sleep(25000);
+    std :: string time = currentDateTime();
+    Sleep(1000);
+    test.create_point("full");
+    std :: cout << test.print_amount_of_points() << '\n';
+    test.print_point_info(0);
+    test.delete_by_date(time);
+    std :: cout << test.print_amount_of_points() << '\n';
+    test.print_point_info(0);
+}
+
+void case4(){
     Back_Up test({"test1.txt","test2.txt"});
     test.create_point("full");
     test.create_point("full");
     test.create_point("full");
     test.create_point("full");
-    Sleep(5000);
-    std :: string time = currentDateTime();
-    test.hybrid_delete(4,-1,time,true,true);
-    std :: cout << test.print_amount_of_points();
-    test.hybrid_delete(4,-1,time,true,false);
+    test.create_point("full");
+    test.create_point("full");
+    test.hybrid_delete(6,2500,"none",true,true);
+}
+
+int main() {
+
     return 0;
 }
